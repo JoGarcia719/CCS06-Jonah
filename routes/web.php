752c40835ea43894b61fe5a773cb43a9070a9877
JoGarcia719 <garcia.josephandrew@auf.edu.ph>
2022-04-04 19:56:48 +0800

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\JonahController;
+Route::get('/', [JonahController::class, 'index']);
+Route::get('/chapter/{chapter_number}', [JonahController::class, "readByChapter"]);
+Route::get('/all-chapters', [JonahController::class, 'readAllChapters']);
